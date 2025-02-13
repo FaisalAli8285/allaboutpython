ac72a02fbd4bb5ea4d8696bedcc312bb2aea48e8
FaisalAli8285 <faisalali159876@gmail.com>
2025-02-13 11:39:25 +0500

    nested loop added

---
+++ nested_loop.py
@@ -0,0 +1,12 @@
+for i in range(1,6):  
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")
+ 
+        #  reverse order
+print("reverse order") 
+
+for i in range(1,6):  
+    for j in range(5,i-1,-1):
+        print(j,end="")
+    print("") 
